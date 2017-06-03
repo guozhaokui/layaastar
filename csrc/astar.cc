b899816bb0a64b3dd25229c1066ae0a99d7263f0
guozhaokui <guozk@layabox.com>
2017-06-03 16:13:52 +0800

    加保护边的方式导致很多容易混淆的地方，例如内外格子概念不一致。所以要去掉了，先备份一下

--- csrc/astar.cc
+++ csrc/astar.cc
@@ -6,7 +6,14 @@
 #include <unordered_map>
 #include <functional>
 
-unsigned char map[] = {
+
+//不可行区域
+#define BLOCKV 0
+
+//可行区域
+#define VALIDV 1
+
+MAPDATATYPE map[] = {
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -85,11 +92,11 @@ void NodeToCoord(const int nNode, Vec2& coord, const int nMapWidth){
 }
 
 int Heuristic(const int nFromX, const int nFromY, const int nToX, const int nToY){
-    return std::abs(nFromX - nToX) + std::abs(nFromY - nToY);
+    //return std::abs(nFromX - nToX) + std::abs(nFromY - nToY);
     int dx = nFromX - nToX;
     int dy = nFromY - nToY;
 
-    return sqrt(dx*dx + dy*dy);
+    return (dx*dx + dy*dy);
 }
 
 int ReconstructPath(const int nStart, const int nTarget, const int nOutBufferSize,
@@ -113,16 +120,16 @@ int ReconstructPath(const int nStart, const int nTarget, const int nOutBufferSiz
 }
 
 int FindPath(const int nStartX, const int nStartY,const int nTargetX, const int nTargetY,
-    const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
+    const MAPDATATYPE* pMap, const int nMapWidth, const int nMapHeight,
     int* pOutBuffer, const int nOutBufferSize){
 
     if ((nStartX == nTargetX) && (nStartY == nTargetY)){
         return 0;
     }
 
-    std::priority_queue<PQElement, std::vector<PQElement>, std::greater<PQElement> > Fringe;//open节点
+    std::priority_queue<PQElement, std::vector<PQElement>, std::greater<PQElement> > OpenSet;
     std::unordered_map<int, int> CameFrom;  //用来倒着生成路径的，记录从哪个节点来的
-    std::unordered_map<int, int> Cost;      //每个节点到起点的实际距离。 g(n)
+    std::unordered_map<int, int> Cost;      //每个节点到起点的实际距离。 对应wiki的 g_score[]
 
     const int nStart = CoordToNode(nStartX, nStartY, nMapWidth);
     const int nTarget = CoordToNode(nTargetX, nTargetY, nMapWidth);
@@ -130,16 +137,16 @@ int FindPath(const int nStartX, const int nStartY,const int nTargetX, const int 
     if (!pMap[nTarget])
         return 0;
 
-    Fringe.emplace(0, nStart);
+    OpenSet.emplace(0, nStart);
     Cost[nStart] = 0;
 
     static const int pMod[] = { -1, -1, 0,-1, 1,-1, -1,0, 1, 0, -1,1, 0,1, 1, 1}; // (x, y) pairs
     Vec2 coord = { 0,0 };
     bool bFound = false;
-    while (!Fringe.empty()){
+    while (!OpenSet.empty()){
         //从openset中取出最好的。
-        const int nCurrent = Fringe.top().second; 
-        Fringe.pop();
+        const int nCurrent = OpenSet.top().second; 
+        OpenSet.pop();
 
         NodeToCoord(nCurrent, coord, nMapWidth);
 
@@ -159,12 +166,13 @@ int FindPath(const int nStartX, const int nStartY,const int nTargetX, const int 
                 pMap[nNeighbour]){
 
                 const int nNewCost = Cost[nCurrent] + 1;
+                //这里并没有使用closeset，因为如果遇到了closeset，一定满足nNewCost < Cost[nNeighbour]
                 if (Cost.find(nNeighbour) == Cost.end() || nNewCost < Cost[nNeighbour]){
                     //计算f(n)
                     const int nPriority = nNewCost + Heuristic(nNewX, nNewY,nTargetX, nTargetY);
                     CameFrom[nNeighbour] = nCurrent;
                     Cost[nNeighbour] = nNewCost;
-                    Fringe.emplace(nPriority, nNeighbour);
+                    OpenSet.emplace(nPriority, nNeighbour);
                 }
             }
         }
@@ -227,4 +235,92 @@ void test() {
         //saveasTxt(stx, sty, edx, edy, obuf, nPathLen);
     }
     printf("OK\n");
+}
+
+/**
+
+*/
+void findPath(int stx, int sty, int edx, int edy, int maxwidth,int maxheight, int linedist) {
+
+}
+
+/*
+ * http://www.cnblogs.com/pheye/archive/2010/08/14/1799803.html
+*/
+void DrawLine(int x1, int y1, int x2, int y2){
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int ux = ((dx > 0) << 1) - 1;//x的增量方向，取或-1
+    int uy = ((dy > 0) << 1) - 1;//y的增量方向，取或-1
+    int x = x1, y = y1, eps;//eps为累加误差
+
+    eps = 0; dx = abs(dx); dy = abs(dy);
+    if (dx > dy){
+        for (x = x1; x != x2; x += ux){
+            //SetPixel(img, x, y);
+            eps += dy;
+            if ((eps << 1) >= dx){
+                y += uy; eps -= dx;
+            }
+        }
+    }
+    else{
+        for (y = y1; y != y2; y += uy){
+            //SetPixel(img, x, y);
+            eps += dx;
+            if ((eps << 1) >= dy){
+                x += ux; eps -= dy;
+            }
+        }
+    }
+}
+
+AStarMap::AStarMap(const MAPDATATYPE* pMap, const int nMapWidth, const int nMapHeight,
+    int nPosX, int nPosY, int nGridWidth, int nGridHeight) {
+    
+    mnWidth = nMapWidth + 2;
+    mnHeight = nMapHeight + 2;
+    mpMap = new MAPDATATYPE[mnWidth*mnHeight];
+    memset(mpMap, BLOCKV, mnWidth*sizeof(MAPDATATYPE));
+    memset(mpMap + mnWidth*(mnHeight-1), BLOCKV, mnWidth*sizeof(MAPDATATYPE));
+    MAPDATATYPE *pMapL = mpMap+mnWidth, *pMapR=mpMap+mnWidth+mnWidth-1;
+    const MAPDATATYPE *pSrcMap = pMap;
+    for (int y = 0; y < mnHeight-2; y++) {
+        *pMapL = BLOCKV;
+        memcpy(pMapL + 1, pSrcMap, nMapWidth*sizeof(MAPDATATYPE));
+        *pMapR = BLOCKV;
+        pSrcMap += nMapWidth;
+        pMapL += mnWidth;
+        pMapR += mnWidth;
+    }
+    mnGridWidth = nGridWidth;
+    mnGridHeight = nGridHeight;
+    mnPosX = nPosX - nGridWidth;
+    mnPosY = nPosY - nGridHeight;
+}
+
+AStarMap::~AStarMap() {
+    if (mpOutBuffer) {
+        delete[] mpOutBuffer;
+    }
+    if (mpMap) {
+        delete[] mpMap;
+    }
+}
+
+int AStarMap::_findPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY) {
+    int nStartGridX = nStartX / mnGridWidth - 1;
+    int nStartGridY = nStartY / mnGridHeight - 1;
+    if (nStartGridX < 0)nStartGridX = 0;
+    if (nStartGridY < 0)nStartGridY = 0;
+    int nTargetGridX = nTargetX / mnGridWidth - 1;
+    int nTargetGridY = nTargetY / mnGridHeight - 1;
+    return _findPathGrid(nStartGridX, nStartGridY, nTargetGridX, nTargetGridY);
+}
+
+int  AStarMap::_findPathGrid(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY) {
+    int sx = nStartX + 1;
+    int sy = nStartY + 1;
+    int ex = nTargetX + 1;
+    int ey = nTargetY + 1;
 }
--- csrc/astar.h
+++ csrc/astar.h
@@ -1,11 +1,52 @@
 #ifndef _LAYA_A_START_H___
 #define _LAYA_A_START_H___
 
+#define MAPDATATYPE unsigned int
+
 int FindPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY,
-    const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
+    const MAPDATATYPE* pMap, const int nMapWidth, const int nMapHeight,
     int* pOutBuffer, const int nOutBufferSize);
 
 void test();
+
+/**
+* 为了提高效率，加了一个保护边。所以不能直接返回格子数据。如果返回位置则没有关系。
+*/
+class AStarMap {
+public:
+    AStarMap(const MAPDATATYPE* pMap, const int nMapWidth, const int nMapHeight, int nPosX, int nPosY, int nGridWidth, int nGridHeight);
+    ~AStarMap();
+
+protected:
+    /**
+     * @param nStartX 开始位置，不是格子，是位置。
+     * @param nTargetX 目标位置，不是格子，是位置。
+    */
+    int _findPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY);
+
+    /**
+    * @param nStartX 开始位置，是格子。 是外部格子，与内部格子的xy都差1
+    * @param nTargetX 目标位置，是格子
+    */
+    int _findPathGrid(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY);
+
+    /**
+     * 直线检测。
+     * @param stx 格子，是本地
+    */
+    int _rayCast(const int stx, const int sty, const int edx, const int edy);
+
+    int* mpOutBuffer=nullptr;
+    int mnOutBufferSz = 0;
+    int mnWidth = 0;
+    int mnHeight = 0;
+    int mnGridWidth = 128;
+    int mnGridHeight = 128;
+    int mnPosX = 0;
+    int mnPosY = 0;
+    MAPDATATYPE* mpMap=nullptr;
+
+};
 
 #endif
 
